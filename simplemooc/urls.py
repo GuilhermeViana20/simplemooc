be942d3c0a829518d23e6894f7e7e41a959a968e
Guilherme Viana <guilherme.viana@kmm.com.br>
2025-05-21 17:35:05 -0300

    Página de cursos dinâmica

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('simplemooc.core.urls')),
+    path('index', include('simplemooc.courses.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
